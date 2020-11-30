c1bdd505052de8a8d45389352f5d6c67c075d0e9
ishan <ishangupta004@gmail.com>
2020-11-30 17:31:56 +0530

    truffle tasks added

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -2,4 +2,21 @@ pragma solidity ^0.5.0;
 
 contract ToDoList {
     uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        createTask("First task in ToDo list");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
