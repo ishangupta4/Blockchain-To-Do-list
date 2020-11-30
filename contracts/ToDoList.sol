9295907aacc7796d57321df66fe86a63953625dc
ishan <ishangupta004@gmail.com>
2020-11-30 20:14:47 +0530

    functionality to check completed tasks added

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -13,6 +13,8 @@ contract ToDoList {
 
     event TaskCreated(uint256 id, string content, bool completed);
 
+    event TaskCompleted(uint256 id, bool completed);
+
     constructor() public {
         createTask("First task in ToDo list");
     }
@@ -21,5 +23,12 @@ contract ToDoList {
         taskCount++;
         tasks[taskCount] = Task(taskCount, _content, false);
         emit TaskCreated(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint256 _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
     }
 }
--- src/app.js
+++ src/app.js
@@ -101,8 +101,8 @@ App = {
       $newTaskTemplate
         .find("input")
         .prop("name", taskId)
-        .prop("checked", taskCompleted);
-      // .on('click', App.toggleCompleted)
+        .prop("checked", taskCompleted)
+        .on("click", App.toggleCompleted);
 
       // Put the task in the correct list
       if (taskCompleted) {
@@ -120,6 +120,13 @@ App = {
     App.setLoading(true);
     const content = $("#newTask").val();
     await App.todoList.createTask(content);
+    window.location.reload();
+  },
+
+  toggleCompleted: async (e) => {
+    App.setLoading(true);
+    const taskId = e.target.name;
+    await App.todoList.toggleCompleted(taskId);
     window.location.reload();
   },
 
